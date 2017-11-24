beb94f655f1c70345b8daf5c6255377cca02082d
Jeremie Elbaz <jeremie@MacBook-Pro-de-Jeremie.local>
2017-11-24 09:26:16 +0100

    Initial Commit

---
+++ Corsicam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Corsicam
+//
+//  Created by Jeremie Elbaz on 24/11/2017.
+//  Copyright © 2017 Jeremie Elbaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
